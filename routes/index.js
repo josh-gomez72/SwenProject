d935434c8f27ea242771ec7ac2781ee1ac4bddec
Josh Gomez <josh.gomez72@gmail.com>
2016-05-10 16:13:44 +1200

    Made basic register page and confirmation page(which is a throwaway page) and also made it so that it will add the user to the database. Still need to do validation on this so that they have to enter in values before continuing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
+var pg = require('pg').native;
 
 var database = "postgres://gomezjosh:password@depot:5432/SwenGroup9";
+var client = new pg.Client(database);
+client.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -30,6 +32,27 @@ pg.connect(database, function(err, client, done){
 
 router.get('/register', function(req, res, next){
 	res.render('register', {title: 'Register User'});
+});
+
+router.get('/register/confirmation', function(req, res, next){
+	//res.send(req);
+	var fname = req.query.firstname;
+	var lname = req.query.lastname;
+	var username = req.query.username;
+	var password = req.query.password;	
+	var email = req.query.email;
+	var address = req.query.address;
+	var city = req.query.city;
+	var postcode = req.query.postcode;
+	var phone = req.query.phoneNumber;
+	
+	console.log(req.query.firstname);
+	var query = client.query("INSERT INTO Users (username, fname, lname, password, email, address, city, postcode, phone) VALUES ('" + username + "' , '" + fname + "' , '" + lname + "' , '" + password + "' , '" + email + "' , '" + address + "' , '" + city + "' , '" + postcode + "' , '" + phone + "');");
+	//Very annoying that I can't put new lines in this command otherwise it cant run because it sees it as an unexpected token
+	//
+	console.log(query.text);	
+	res.render('regconfirm', {title: 'Confirmation!'});
+	console.log("Something happened");
 });
 
 module.exports = router;

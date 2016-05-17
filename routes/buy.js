271f7d817a6910a18ca737977b24a4e41ac25615
CDeighton <cullumdeighton@gmail.com>
2016-05-17 16:53:20 +1200

    Made the carousel in the buying page autoload user images

--- routes/buy.js
+++ routes/buy.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('buy', { title: 'Farmers Market' });
 });
 
+router.post('/addToCart', function(req, res, next) {
+  console.log(JSON.stringify(req.body));
+  res.write("<strong>Success!</strong> Item added to cart.");
+  res.end();
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,33 +4,33 @@ var router = express.Router();
 var pg = require('pg'); //used for windows
 
 // var database = "postgres://gomezjosh:password@depot:5432/SwenGroup9";
- var database = "postgres://tihxgzxemzbafr:hiCzGMi1vENgac3Cmd-UyZDeZ-@ec2-54-235-208-3.compute-1.amazonaws.com:5432/defa0fcjs2b02k";
- // var client = new pg.Client(database);
- // client.connect();
+ var database = "postgres://tihxgzxemzbafr:hiCzGMi1vENgac3Cmd-UyZDeZ-@ec2-54-235-208-3.compute-1.amazonaws.com:5432/defa0fcjs2b02k?ssl=true";
+ var client = new pg.Client(database);
+ client.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.redirect('/login');
 });
 
- // pg.connect(database, function(err, client, done){
- // 	if(err){
- // 		console.error('Could not connect to the database');
- // 		console.error(err);
- // 		return;
- // 	}
- //
- // 	console.log('Connected to database: SwenGroup9');
- // 	client.query("SELECT * FROM Users;", function(error, result){
- // 	done();
- // 	if (error){
- // 		console.error('Failed to execute query');
- // 		console.error(error);
- // 		return;
- // 		}
- // 	console.log(result);
- // 	});
- // });
+ pg.connect(database, function(err, client, done){
+ 	if(err){
+ 		console.error('Could not connect to the database');
+ 		console.error(err);
+ 		return;
+ 	}
+
+ 	console.log('Connected to database: SwenGroup9');
+ 	client.query("SELECT * FROM Users;", function(error, result){
+ 	done();
+ 	if (error){
+ 		console.error('Failed to execute query');
+ 		console.error(error);
+ 		return;
+ 		}
+ 	console.log(result);
+ 	});
+ });
 
 router.get('/register', function(req, res, next){
 	res.render('register', {title: 'Register User'});

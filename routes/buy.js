4a5d3335042084273c1bff92623d8cb09f30d979
CDeighton <cullumdeighton@gmail.com>
2016-05-17 22:02:16 +1200

    Fixed bug where image would upload but not show its thumbnail

--- routes/buy.js
+++ routes/buy.js
@@ -17,20 +17,15 @@ router.post('/item', function(req,res,next){
   console.log(JSON.stringify(req.body));
 
   client.query("SELECT * FROM Items WHERE itemid="+req.body.itemID+";", function(error, result){
-    // done();
     if (error){
       console.error('Failed to execute query');
       console.error(error);
       return;
     }
     console.log("RESULT: " + JSON.stringify(result.rows[0]));
-    console.log("RESULT: " + JSON.parse(result.rows[0].image)[0]);
     res.write(JSON.stringify(result.rows[0]));
     res.end();
   });
-
-  // res.write(JSON.stringify({title:"ECS", desc: "A great deal of disappointment",price: 77, images:["/images/uploads/19574e1c2d3aea60ae72a43a6e016491.png","/images/uploads/1c8f360e53886b6e6f0352ea3d38a156.png"]}));
-  // res.end();
 });
 
 router.post('/addToCart', function(req, res, next) {
--- routes/sell.js
+++ routes/sell.js
@@ -13,55 +13,61 @@ client.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('sell', { title: 'Farmers Market' });
+    res.render('sell', { title: 'Farmers Market' });
 });
 
 var upload = multer({dest: './uploads/'});
 router.post('/uploadPhoto', upload.single('fileToUpload'), function(req, res) {
-  console.log("file: " + JSON.stringify(req.file));
-  fs.createReadStream(req.file.path).pipe(fs.createWriteStream('../public/images/uploads/'+req.file.filename+".png"));
-  fs.unlink(req.file.path, function(err) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log("File deleted successfully!");
-  });
-  res.write("/images/uploads/"+req.file.filename+".png");
-  
-  res.end();
+    console.log("file: " + JSON.stringify(req.file));
+    var pipe = fs.createReadStream(req.file.path).pipe(fs.createWriteStream('../public/images/uploads/'+req.file.filename+".png"));
+    pipe.on('finish', function () {
+        res.write("/images/uploads/"+req.file.filename+".png");
+        res.end();
+    });
+    fs.unlink(req.file.path, function(err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("File deleted successfully!");
+
+    });
 });
 
 router.post('/list', function(req,res){
-  console.log("LIST: "+req.body.title);
-  console.log("LIST: "+req.body.desc);
-  console.log("LIST: "+req.body.price);
-  console.log("LIST: "+req.body.images);
+    console.log("LIST: "+req.body.title);
+    console.log("LIST: "+req.body.desc);
+    console.log("LIST: "+req.body.price);
+    console.log("LIST: "+req.body.images);
 
-  var input = "VALUES('"+req.body.title+"','"+
-      req.body.desc+"'," +
-      "'misc','" +
-      req.body.price+"','" +
-      JSON.stringify(req.body.images)+"','" +
-      10+"'," +
-      "'SmithBob'," +
-      "'ULTRA MISC'" +
-      ");"
-  console.log("INPUT: " + input);
-  res.write("Hey");
-  // VALUES("+req.body.title+","+req.body.desc+",human,"+req.body.price+","+req.body.images+",10,Jesus,God)
-  client.query("INSERT INTO Items(name,description,category,price,image,stock,seller,parent_category)" +
-      input, function(error, result){
-    // done();
-    if (error){
-      console.error('Failed to execute query');
-      console.error(error);
-      return;
-    }
-    console.log("RESULT: " + JSON.stringify(result.rows));
-  });
+    var input = "VALUES('"+req.body.title+"','"+
+        req.body.desc+"'," +
+        "'misc','" +
+        req.body.price+"','" +
+        JSON.stringify(req.body.images)+"','" +
+        10+"'," +
+        "'SmithBob'," +
+        "'ULTRA MISC'" +
+        ");"
+    console.log("INPUT: " + input);
+    // VALUES("+req.body.title+","+req.body.desc+",human,"+req.body.price+","+req.body.images+",10,Jesus,God)
+    client.query("INSERT INTO Items(name,description,category,price,image,stock,seller,parent_category)" +
+        input, function(error, result){
+        // done();
+        if (error){
+            console.error('Failed to execute query');
+            console.error(error);
+            res.write(JSON.stringify({success:false,message:"<strong>Error: </strong> Item unable to be listed."}));
+            res.end();
+            return;
+        }
+        console.log("RESULT: " + JSON.stringify(result.rows));
+
+        res.write(JSON.stringify({success:true,message:"<strong>Success!</strong> Item listed."}));
+        res.end();
+    });
 
 
-  res.end();
+
 });
 
 module.exports = router;

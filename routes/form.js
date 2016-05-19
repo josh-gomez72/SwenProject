26197765c69e29d10fe1da6626ed126e981efb51
Iris Riddiford Graham <irisjo.rg@gmail.com>
2016-05-19 14:28:44 +1200

    With Navbar and front end browse

--- routes/form.js
+++
@@ -1,6 +0,0 @@
-$(document).on("ready", function(){
-	$('#search-form').submit(function(event){
-		event.preventDefult();
-		console.log("searched");
-	});
-});
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,48 @@
 var express = require('express');
 var router = express.Router();
+//var pg = require('pg').native; //used for lab machines
+var pg = require('pg'); //used for windows
 
-/* GET home page. */
+//var database = "postgres://gomezjosh:password@depot:5432/SwenGroup9";
+var database = "postgres://tihxgzxemzbafr:hiCzGMi1vENgac3Cmd-UyZDeZ-@ec2-54-235-208-3.compute-1.amazonaws.com:5432/defa0fcjs2b02k?ssl=true";
+var client = new pg.Client(database);
+client.connect();
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.redirect('/browse');
+});
+
+ pg.connect(database, function(err, client, done){
+ 	if(err){
+ 		console.error('Could not connect to the database');
+ 		console.error(err);
+ 		return;
+ 	}
+
+ 	console.log('Connected to database: SwenGroup9');
+ 	client.query("SELECT * FROM Users;", function(error, result){
+ 	done();
+ 	if (error){
+ 		console.error('Failed to execute query');
+ 		console.error(error);
+ 		return;
+ 		}
+ 	console.log(result);
+ 	});
+ });
+
+router.get('/browse', function(req, res, next) {
+  res.render('browse', {title: 'Browse', resultPlace: {}});
 });
 
 router.get('/browse/:catagory', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var header = 'Browse in ' + req.params.catagory;
+	res.render('browse', {title: 'Browse'});
 });
 
-
-/* GET search page. */
-router.get('/search', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/browse-location/:location', function(req, res, next) {
+	var header = 'Browse in ' + req.params.location;
+	res.render('browse', {title: 'Browse'});
+});;
 
 module.exports = router;

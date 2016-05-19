f73890b8f590428b4b4197fdb3a5992a1db065d0
Josh Gomez <josh.gomez72@gmail.com>
2016-05-19 15:22:07 +1200

    Fixed merge errors

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-<<<<<<< HEAD
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-=======
+  });
+
 router.get('/users', function(req, res, next) {
  // res.send('respond with a resource');
   res.render('sell', { title: 'Farmers Market' });
->>>>>>> origin/master
 });
 
 module.exports = router;
